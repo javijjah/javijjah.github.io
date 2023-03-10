c4f53594d1702fb3b8a33d8b8ca74a34205a62f8
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2023-03-10 21:13:39 +0100

    empieza js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function lighttheme(){
     let lighttheme=document.getElementById("html").className='lighttheme';
-    
-    }
+}
 function darktheme(){
 let darktheme=document.getElementById("html").className='darktheme';
 }
