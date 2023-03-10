b775658b00017bd718c26af29df762cd9025b406
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2023-03-10 10:16:18 +0100

    Cambio de tema

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function lighttheme(){
+    let lighttheme=document.getElementById("html").className='lighttheme';
+    
+    }
+function darktheme(){
+let darktheme=document.getElementById("html").className='darktheme';
+}
+function pinktheme(){
+let pinktheme=document.getElementById("html").className='pinktheme';
+}
